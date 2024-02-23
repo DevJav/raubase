59eaa1ed49b6e740052ed43d5b049cb8a590b844
June <june_robot@dtu.dk>
2024-02-23 14:39:19 +0100

    Basic line follow

--- src/bplan41.cpp
+++ src/bplan41.cpp
@@ -78,256 +78,84 @@ void BPlan41::run()
   if (ini["Plan41"]["run"] == "false")
     return;
   UTime t("now");
-  bool finished = false;
-  bool lost = false;
-  state = 1;
-  oldstate = state;
   const int MSL = 100;
   char s[MSL];
   //
   toLog("Plan41 started");
   //
 
-  float base_velocity = 0.2;
-  float P = 100.0;
-  int last_state = 0;
+  float base_velocity = 0.25;
+  float turn_velocity = 0.6;
+  int last_state = 4;
+  int lost_counter = 0;
 
-  while (not finished and not lost and not service.stop)
+  while (not service.stop)
   {
-    switch (state)
+    if (medge.edgeValid)
     {
-    case 1:
-      if (medge.edgeValid)
-      {
-        float dist_right_edge = medge.rightEdge;
-        float dist_left_edge = medge.leftEdge;
+      float dist_right_edge = medge.rightEdge;
+      float dist_left_edge = medge.leftEdge;
 
-        std::cout << "Right edge: " << dist_right_edge << std::endl;
-        std::cout << "Left edge: " << dist_left_edge << std::endl;
+      // std::cout << "Right edge: " << dist_right_edge << std::endl;
+      // std::cout << "Left edge: " << dist_left_edge << std::endl;
 
-        if (dist_right_edge < 0.0 and dist_left_edge > 0.0 && last_state != 0)
-        {
-          std::cout << "Both edges are found" << std::endl;
-          mixer.setRightVelocity(base_velocity);
-          mixer.setLeftVelocity(base_velocity);
-          last_state = 0;
-        }
-        else if (dist_right_edge > 0.0 && last_state != 1)
-        {
-          std::cout << "Correcting right edge" << std::endl;
-          mixer.setRightVelocity(base_velocity + (P * abs(dist_right_edge)));
-          mixer.setLeftVelocity(base_velocity - (P * abs(dist_right_edge)));
-          last_state = 1;
-        }
-        else if (dist_left_edge < 0.0 && last_state != 2)
-        {
-          std::cout << "Correcting left edge" << std::endl;
-          mixer.setRightVelocity(base_velocity - (P * abs(dist_left_edge)));
-          mixer.setLeftVelocity(base_velocity + (P * abs(dist_left_edge)));
-          last_state = 2;
-        }
-      }
-      break;
-
-    case 5: // wait for Regbot, then go forward
-      if (1)
-      { // something is close, assume it is the Regbot
-        // start driving
-        pose.resetPose();
-        toLog("forward 0.1 m/sec");
-        mixer.setVelocity(0.1);
-        mixer.setTurnrate(0);
-        state = 12;
-      }
-      else if (t.getTimePassed() > 10)
+      if ((dist_right_edge < 0.0 && dist_left_edge > 0.0) && last_state != 0)
       {
-        toLog("Gave up waiting for Regbot");
-        lost = true;
+        std::cout << "Both edges are found" << std::endl;
+        mixer.setManualControl(true, base_velocity, 0.0);
+        last_state = 0;
       }
-      break;
-    case 12: // forward until distance, then look for edge
-      if (pose.dist > 0.3)
+      else if ((dist_right_edge > 0.0) && last_state != 1)
       {
-        toLog("Continue until edge is found");
-        state = 20;
-        pose.dist = 0;
+        std::cout << "Correcting right edge" << std::endl;
+        mixer.setManualControl(true, base_velocity, turn_velocity);
+        last_state = 1;
       }
-      else if (t.getTimePassed() > 10)
-      { // line should be found within 10 seconds, else lost
-        toLog("failed to find line after 10 sec");
-        lost = true;
-      }
-      break;
-    case 20: // forward looking for line, then turn
-      if (medge.width > 0.05)
+      else if ((dist_left_edge < 0.0) && last_state != 2)
       {
-        toLog("found line, turn left");
-        // set to edge control, left side and 0 offset
-        mixer.setVelocity(0.2); // slow
-        mixer.setTurnrate(1.0); // rad/s
-        state = 30;
-        pose.dist = 0;
-        pose.turned = 0;
+        std::cout << "Correcting left edge" << std::endl;
+        mixer.setManualControl(true, base_velocity, -turn_velocity);
+        last_state = 2;
       }
-      else if (t.getTimePassed() > 10 or pose.dist > 0.6)
-      { // line should be found within 10 seconds, else lost
-        toLog("failed to find line after 10 sec / 30cm");
-        lost = true;
-      }
-      break;
-
-    case 21:                  // forward looking for line, then turn
-      mixer.setVelocity(0.5); // slow
-      mixer.setTurnrate(0.0); // rad/s
-
-      toLog("Case 21");
-      snprintf(s, MSL, "r = %.4f, l = %.4f", medge.rightEdge, medge.leftEdge);
-      toLog(s);
-      if (medge.width > 0.015)
-      {
-
-        state = 21;
-        // set to edge control, left side and 0 offset
-        if (medge.edgeValid and medge.rightEdge > 0.0)
-        {
-          state = 22;
-        }
-        if (medge.edgeValid and medge.leftEdge < 0.0)
-        {
-          state = 23;
-        }
-        pose.dist = 0;
-        pose.turned = 0;
-      }
-      // else if (t.getTimePassed() > 10 or pose.dist > 0.6)
-      // { // line should be found within 10 seconds, else lost
-      //   toLog("failed to find line after 10 sec / 30cm");
-      //   lost = true;
-      // }
-      break;
-    case 22: // Correct right edge
-
-      toLog("Case 22");
-      mixer.setVelocity(0.2); // slow
-      mixer.setTurnrate(0.4); // rad/s
-      if (medge.edgeValid and medge.rightEdge < -0.005)
-      {
-        state = 21;
-      }
-      break;
-
-    case 23: // Correct left edge
-
-      toLog("Case 23");
-      mixer.setVelocity(0.2);  // slow
-      mixer.setTurnrate(-0.4); // rad/s
-      if (medge.edgeValid and medge.leftEdge > 0.005)
-      {
-        state = 21;
-      }
-      break;
-
-    case 30: // Continue turn until right edge is almost reached, then follow right edge
-      if (medge.edgeValid and medge.rightEdge > -0.04 and pose.turned > 0.3)
-      {
-        toLog("Line detected, that is OK to follow");
-        mixer.setEdgeMode(false /* right */, -0.03 /* offset */);
-        mixer.setVelocity(0.3);
-        state = 40;
-        pose.dist = 0;
-      }
-      else if (t.getTimePassed() > 10)
-      {
-        toLog("Time passed, no crossing line");
-        lost = true;
-      }
-      else if (pose.dist > 1.0)
-      {
-        toLog("Driven too long");
-        state = 90;
-      }
-      break;
-    case 40: // follow edge until crossing line, the go straight
-      if (medge.width > 0.075 and pose.dist > 0.2)
-      { // go straight
-        toLog("case 40");
-        mixer.setTurnrate(0);
-        pose.dist = 0;
-        state = 50;
-      }
-      else if (t.getTimePassed() > 10)
-      {
-        toLog("too long time");
-        finished = true;
-      }
-      else if (not medge.edgeValid)
-      {
-        toLog("Lost line");
-        state = 80;
-      }
-      break;
-    case 50: // continue straight until wall is close
-      if (dist.dist[0] < 0.35)
-      { // wall found
-        toLog("wall found");
-        mixer.setVelocity(0);
-        finished = true;
-      }
-      else if (t.getTimePassed() > 10)
-      {
-        toLog("too long time");
-        lost = true;
-      }
-      else if (pose.dist > 1.5)
-      {
-        toLog("too far");
-        lost = true;
-      }
-      break;
-    default:
-      lost = true;
-      break;
     }
-    if (state != oldstate)
-    { // C-type string print
-      snprintf(s, MSL, "State change from %d to %d", oldstate, state);
-      toLog(s);
-      oldstate = state;
-      t.now();
+    else
+    {
+      if (last_state != 4 && lost_counter > 10)
+      {
+        std::cout << "Lost" << std::endl;
+        mixer.setManualControl(true, 0.0, 0.0);
+        last_state = 4;
+        lost_counter = 0;
+      }
+      else
+      {
+        lost_counter++;
+      }
     }
-    // wait a bit to offload CPU (4000 = 4ms)
     usleep(4000);
   }
-  if (lost)
-  { // there may be better options, but for now - stop
-    toLog("Plan41 got lost - stopping");
-    mixer.setVelocity(0);
-    mixer.setTurnrate(0);
-  }
-  else
-    toLog("Plan41 finished");
 }
 
-void BPlan41::terminate()
-{ //
-  if (logfile != nullptr)
-    fclose(logfile);
-  logfile = nullptr;
-}
+  void BPlan41::terminate()
+  { //
+    if (logfile != nullptr)
+      fclose(logfile);
+    logfile = nullptr;
+  }
 
-void BPlan41::toLog(const char *message)
-{
-  UTime t("now");
-  if (logfile != nullptr)
+  void BPlan41::toLog(const char *message)
   {
-    fprintf(logfile, "%lu.%04ld %d %% %s\n", t.getSec(), t.getMicrosec() / 100,
-            oldstate,
-            message);
+    UTime t("now");
+    if (logfile != nullptr)
+    {
+      fprintf(logfile, "%lu.%04ld %d %% %s\n", t.getSec(), t.getMicrosec() / 100,
+              oldstate,
+              message);
+    }
+    if (toConsole)
+    {
+      printf("%lu.%04ld %d %% %s\n", t.getSec(), t.getMicrosec() / 100,
+             oldstate,
+             message);
+    }
   }
-  if (toConsole)
-  {
-    printf("%lu.%04ld %d %% %s\n", t.getSec(), t.getMicrosec() / 100,
-           oldstate,
-           message);
-  }
-}
